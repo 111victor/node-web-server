162035363714456fba9aeb7e837517a3ea552059
qingyang <1234567victor@163.com>
2018-10-27 12:57:01 -0700

    add project page

--- server.js
+++ server.js
@@ -54,8 +54,13 @@ app.get('/',(req,res) => {
 
 app.get('/about',(req,res)=>{
 	res.render('about.hbs',{
-		pageTitle: 'About Page',
-		currentYear: new Date().getFullYear()
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/projects',(req,res)=>{
+	res.render('projects.hbs',{
+		pageTitle: 'Projects'
 	});
 });
 
